47296a4a054496cf1d894d7eb607505f0f9c9c1d
xz1Bit3 <nadolko.kd@gmail.com>
2024-09-26 15:32:27 +0300

    Add files via upload

--- module_3.1.py
+++ module_3.1.py
@@ -1,36 +1,30 @@
 calls = 0
 
 
-def count_calls ():
+def count_calls():
     global calls
     calls += 1
 
 
 def string_info(string):
     count_calls()
-    return len(string), string.upper(), string.lower()
+    tuple_new = (len(string), string.upper(), string.lower())
+    return tuple_new
+
 
 def is_contains(string, list_to_search):
     count_calls()
-    for i in list_to_search:
-        print(string.lower(), i.lower(), string.lower() == i)
-    if string.lower() == i.lower():
-        if string == i:
-            return True #завершение функции
+    for item in list_to_search:
+        list_new = []
+        list_new.append(item.lower())
+    if string.lower() in list_new:
+        return True
+    else:
         return False
 
-result = string_info('example')
-result1 = string_info('test')
-#print(result)
-#print(result1)
-#print(calls)
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
-
-
-
-
-
