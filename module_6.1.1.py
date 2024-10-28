574232facb393d20a1f0a048694a36f70bd2fc3f
xz1Bit3 <nadolko.kd@gmail.com>
2024-10-28 19:12:46 +0300

    Add files via upload

---
+++ module_6.1.1.py
@@ -0,0 +1,54 @@
+class Animal:
+    alive = True  #живой
+    fed = False  # сытый
+    def __init__(self,name):
+        self.name = name
+
+    def eat(self,food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
+
+class Plant:
+    edible = False #съедобный
+    def __init__(self,name):
+        self.name = name
+
+
+
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+
+class Flower(Plant):
+    pass
+ #   edible = True
+
+
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+
