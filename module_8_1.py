21000d54066a21d5b2581b210c8f7042a4a7934b
xz1Bit3 <nadolko.kd@gmail.com>
2024-11-18 17:24:35 +0300

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -1,14 +1,8 @@
 def add_everything_up(a, b):
     try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
-        elif isinstance(a, str) and isinstance(b, str):
-            return f'{a} + {b}'
-        else:
-            raise TypeError ('Типы аргументов не совместимы')
-    except TypeError as exc:
-        print(exc)
-        return None
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
