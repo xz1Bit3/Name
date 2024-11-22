e111b9dddb67d3f43b0a3cc5c287ebaadd3884a0
xz1Bit3 <nadolko.kd@gmail.com>
2024-11-22 08:56:59 +0300

    Add files via upload

--- module_9_1.py
+++ module_9_1.py
@@ -8,50 +8,17 @@
 
 
 
+
 def apply_all_func(int_list, *functions):
     results = {}
     for func in functions:
-        # name = func.__name__
-        # result = func(int_list)
-        results.update({func.__name__: func(int_list)})
+        try:
+            result = func(int_list)
+            results[func.__name__] = result
+        except (TypeError, ValueError):
+            results[func.__name__] = 'Функция не может быть применена к списку'
+
     return results
-
-int_list = [12, 4, 75.67, -45, 8.846465849]
-
-def min(int_list):
-    i = 0
-    for i in range(len(int_list)-1):
-        if int_list[i] < int_list[i + 1]:
-            result = int_list[i]
-    return result
-
-def max(int_list):
-    i = 0
-    for i in range(len(int_list)-1):
-        if int_list[i] > int_list[i + 1]:
-            result = int_list[i]
-    return result
-
-def len_(int_list):
-    result = len(int_list)
-    return result
-
-def sum(int_list):
-    result = 0
-    for i in int_list:
-        result += i
-    return result
-
-def sorted_(int_list):
-    result = sorted(int_list)
-    return result
-
-def midle(int_list):
-    result = 0
-    for i in int_list:
-        result += i
-    result = result / len_(int_list)
-    return result
 
 
 print(apply_all_func([6, 20, 15, 9], max, min))
