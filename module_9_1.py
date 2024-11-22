410c1bf1711961530c2a9c8d4f0241da21118708
xz1Bit3 <nadolko.kd@gmail.com>
2024-11-22 08:45:44 +0300

    Add files via upload

--- module_9_1.py
+++ module_9_1.py
@@ -9,10 +9,49 @@
 
 
 def apply_all_func(int_list, *functions):
-    result = {}
-    for i in functions:
-        result[str(i.__name__)] = i(int_list)
-        return result
+    results = {}
+    for func in functions:
+        # name = func.__name__
+        # result = func(int_list)
+        results.update({func.__name__: func(int_list)})
+    return results
+
+int_list = [12, 4, 75.67, -45, 8.846465849]
+
+def min(int_list):
+    i = 0
+    for i in range(len(int_list)-1):
+        if int_list[i] < int_list[i + 1]:
+            result = int_list[i]
+    return result
+
+def max(int_list):
+    i = 0
+    for i in range(len(int_list)-1):
+        if int_list[i] > int_list[i + 1]:
+            result = int_list[i]
+    return result
+
+def len_(int_list):
+    result = len(int_list)
+    return result
+
+def sum(int_list):
+    result = 0
+    for i in int_list:
+        result += i
+    return result
+
+def sorted_(int_list):
+    result = sorted(int_list)
+    return result
+
+def midle(int_list):
+    result = 0
+    for i in int_list:
+        result += i
+    result = result / len_(int_list)
+    return result
 
 
 print(apply_all_func([6, 20, 15, 9], max, min))
