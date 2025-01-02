dbe61dfe57bc67bd9a48558a8c558143294e90ac
xz1Bit3 <nadolko.kd@gmail.com>
2025-01-02 19:54:35 +0300

    Update module_12_2_test.py

--- module_12_2_test.py
+++ module_12_2_test.py
@@ -1,34 +1,47 @@
 import unittest
-import module_12_2
-class RunnerTest(unittest.TestCase):
-    is_frozen = False
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
-    def test_walk(self):
-        runner = module_12_2.Runner('John')
-        for _ in range(10):
-            runner.walk()
-        self.assertEqual(runner.distance, 50)
-    @unittest.skipIf(is_frozen,'Тесты в этом кейсе заморожены')
-    def test_run(self):
-        runner = module_12_2.Runner('Harry')
-        for _ in range(10):
-            runner.run()
-        self.assertEqual(runner.distance, 100)
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
-    def test_challenge(self):
-        runner1 = module_12_2.Runner('John')
-        runner2 = module_12_2.Runner('Harry')
-        for _ in range(10):
-            runner1.run()
-            runner2.walk()
-        self.assertNotEqual(runner1.distance, runner2.distance)
+    def run(self):
+        self.distance += self.speed * 2
 
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
 
 class TournamentTest(unittest.TestCase):
     all_results = {}
-    is_frozen = False
+
     @classmethod
     def setUpClass(cls):
         # Инициализация словаря для хранения результатов
@@ -36,14 +49,13 @@ class TournamentTest(unittest.TestCase):
 
     def setUp(self):
         # Создание объектов бегунов
-        self.runner1 = module_12_2.Runner("Усэйн", speed=10)
-        self.runner2 = module_12_2.Runner("Андрей", speed=9)
-        self.runner3 = module_12_2.Runner("Ник", speed=3)
+        self.runner1 = Runner("Усэйн", speed=10)
+        self.runner2 = Runner("Андрей", speed=9)
+        self.runner3 = Runner("Ник", speed=3)
 
-    @unittest.skipUnless(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_race_1(self):
         # Забег: Усэйн и Ник
-        tournament = module_12_2.Tournament(distance=90)
+        tournament = Tournament(distance=90)
         tournament.participants.append(self.runner1)  # Усэйн
         tournament.participants.append(self.runner3)  # Ник
 
@@ -57,10 +69,9 @@ class TournamentTest(unittest.TestCase):
         last_race_result = results[max(results.keys())]
         self.assertTrue(last_race_result.name == 'Ник')
 
-    @unittest.skipUnless(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_race_2(self):
         # Забег: Андрей и Ник
-        tournament = module_12_2.Tournament(distance=90)
+        tournament = Tournament(distance=90)
         tournament.participants.append(self.runner2)  # Андрей
         tournament.participants.append(self.runner3)  # Ник
 
@@ -74,10 +85,9 @@ class TournamentTest(unittest.TestCase):
         last_race_result = results[max(results.keys())]
         self.assertTrue(last_race_result.name == "Ник")
 
-    @unittest.skipUnless(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_race_3(self):
         # Забег: Усэйн, Андрей и Ник
-        tournament = module_12_2.Tournament(distance=90)
+        tournament = Tournament(distance=90)
         tournament.participants.append(self.runner1)  # Усэйн
         tournament.participants.append(self.runner2)  # Андрей
         tournament.participants.append(self.runner3)  # Ник
@@ -99,6 +109,7 @@ class TournamentTest(unittest.TestCase):
         for k, v in cls.all_results.items():
             formatted_results = {place: str(runner) for place, runner in v.items()}
             print(f"{formatted_results}")
+
 
 if __name__ == '__main__':
     unittest.main()
