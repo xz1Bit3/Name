3f93dffb3847a529e4599448ef3c90e42cc62429
xz1Bit3 <nadolko.kd@gmail.com>
2025-01-06 18:46:45 +0300

    Update module_13_3.py

---
+++ module_13_3.py
@@ -0,0 +1,28 @@
+from lib2to3.btm_utils import tokens
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token =api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+
+@dp.message_handler(text= ['Привет'])
+async def urban_message(message):
+    print('Intro message')
+    await message.answer('Введите команду /start что бы начать общение')
+
+
+
+@dp.message_handler(commands= ['start'])
+async def start_message(message):
+    print('Start message')
+    await message.answer('Привет, я бот помогающий твоему здоровью!')
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates =True)
