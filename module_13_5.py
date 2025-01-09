b13992f7e6ff63df6088186ef589661e558196cb
xz1Bit3 <nadolko.kd@gmail.com>
2025-01-09 17:25:28 +0300

    Update module_13_5.py

--- module_13_5.py
+++ module_13_5.py
@@ -7,7 +7,7 @@ import asyncio
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 
-api = '7727742436:AAG2w-_xHnpKZwcCPdNy0lJWjzoI-436Ct4'
+api = ''
 bot = Bot(token =api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
